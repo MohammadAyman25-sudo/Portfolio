4735010f0f0d10c180013d9e37680bd4d80a5981
Mohammad Ayman <60328501+MohammadAyman25-sudo@users.noreply.github.com>
2025-01-29 14:56:07 +0200

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let readMoreBtn = document.querySelectorAll(".read-more");
+let projectTools = document.querySelectorAll(
+  "#projects .row .col .card .tools"
+);
+let projectAbout = document.querySelectorAll("#projects .row .col .card .card-body .card-text");
+
+projectAbout.forEach(element=>{
+    let idx = Array.prototype.indexOf.call(projectAbout, element);;
+    if(element.children[0].innerText !== "") {
+        readMoreBtn[idx].style.display = "block";
+    }
+});
+
+projectTools.forEach((element) => {
+  let idx = Array.prototype.indexOf.call(projectTools, element);
+  if (element.children[0].innerText !== "") {
+    readMoreBtn[idx].style.display = "block";
+  }
+});
+
+readMoreBtn.forEach(element => {
+    let idx = Array.prototype.indexOf.call(readMoreBtn, element);
+    element.onclick = ()=>{
+        if(element.innerText === "Read More") {
+            projectAbout[idx].style.height = "fit-content";
+            projectTools[idx].style.height = "fit-content";
+            element.innerText = "Read Less";
+        }else {
+            projectAbout[idx].style.height = "70px";
+            projectTools[idx].style.height = "35px";
+            element.innerText = "Read More";
+        }
+    };
+});
